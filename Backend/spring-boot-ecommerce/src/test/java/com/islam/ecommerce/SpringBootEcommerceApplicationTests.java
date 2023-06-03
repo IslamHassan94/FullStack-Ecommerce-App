43adee4186ce70e5a555a62e017abdfdfb90003d
IslamHassan94 <islamhasan@hotmail.com>
2023-06-04 00:57:08 +0300

    first commit

---
+++ Backend/spring-boot-ecommerce/src/test/java/com/islam/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islam.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
